e11ee24320cc658ebb8507aa3a8d3f357a82efcc
zainab-frfr <135985226+zainab-frfr@users.noreply.github.com>
2024-06-15 13:45:33 +0500

    Dialogue boxes UI done

--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rsvp/components/button.dart';
 import 'package:rsvp/components/drawer.dart';
 import 'package:rsvp/components/textfield.dart';
+import 'package:rsvp/themes/themeprovider.dart';
 
 class MyEventsPage extends StatelessWidget {
   MyEventsPage({super.key});
@@ -12,24 +14,89 @@ class MyEventsPage extends StatelessWidget {
   final TextEditingController _date = TextEditingController();
   final TextEditingController _time = TextEditingController();
 
-  Future<void> _showDatePicker(BuildContext context ) async{
-    DateTime? picked = await showDatePicker(
-      context: context, 
-      firstDate: DateTime(2024), 
-      lastDate: DateTime(2030),
+  Future<void> _showDatePicker(BuildContext context) async {
+    bool isDarkMode =
+        Provider.of<ThemeProvider>(context, listen: false).isDarkMode();
+
+    ColorScheme colScme = isDarkMode
+        ? ColorScheme.dark(
+            primary:
+                Theme.of(context).colorScheme.tertiary, // Selected date color
+          )
+        : ColorScheme.light(
+            primary:
+                Theme.of(context).colorScheme.tertiary, // Selected date color
+          );
+
+    TextButtonThemeData textButtonTheme = TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Theme.of(context)
+            .colorScheme
+            .tertiary, // Cancel and OK button text color
+      ),
     );
-    if (picked != null){
+
+    DateTime? picked = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2024),
+      lastDate: DateTime(2030),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData(
+            colorScheme: colScme,
+            textButtonTheme: textButtonTheme,
+            dialogBackgroundColor: Theme.of(context).colorScheme.primary,
+          ),
+          child: child!,
+        );
+      },
+    );
+    if (picked != null) {
       _date.text = "${picked.toLocal()}".split(' ')[0];
     }
   }
 
-  Future<void> _showTimePicker(BuildContext context ) async{
-    TimeOfDay? picked = await showTimePicker(
-    
-      context: context, 
-      initialTime: TimeOfDay.now(), 
+  Future<void> _showTimePicker(BuildContext context) async {
+    bool isDarkMode =
+        Provider.of<ThemeProvider>(context, listen: false).isDarkMode();
+
+    ColorScheme colScme = isDarkMode
+        ? ColorScheme.dark(
+            primary:
+                Theme.of(context).colorScheme.tertiary, // Selected date color
+          )
+        : ColorScheme.light(
+            primary:
+                Theme.of(context).colorScheme.tertiary, // Selected date color
+          );
+
+    TextButtonThemeData textButtonTheme = TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Theme.of(context)
+            .colorScheme
+            .tertiary, // Cancel and OK button text color
+      ),
     );
-    if (picked != null){
+
+    TimePickerThemeData timePickerTheme = TimePickerThemeData(
+      dayPeriodColor: Theme.of(context).colorScheme.tertiary,
+    );
+
+    TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData(
+            colorScheme: colScme,
+            textButtonTheme: textButtonTheme,
+            timePickerTheme: timePickerTheme,
+          ),
+          child: child!,
+        );
+      },
+    );
+    if (picked != null) {
       _time.text = "${picked.hour}:${picked.minute}";
     }
   }
@@ -42,28 +109,47 @@ class MyEventsPage extends StatelessWidget {
             title: const Text('Create Event'),
             backgroundColor: Theme.of(context).colorScheme.primary,
             content: Container(
-              height: 400,
-              width: 300,
-              child: Column(
-                children: [
-                  const SizedBox(height: 20,),
-                  MyTextField(hintText: 'title', obscureText: false, controller: _title),
-                  const SizedBox(height: 20,),
-                  MyTextField(hintText: 'description', obscureText: false, controller: _description),
-                  const SizedBox(height: 20,),
-                  MyTextField(hintText: 'venue', obscureText: false, controller: _venue),
-                  const SizedBox(height: 30,),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MyButton(text: 'Date', onTap: () => _showDatePicker(context)),
-                      MyButton(text: 'Time', onTap: () => _showTimePicker(context)),
-                    ],
-                  )
-                  
-              ],
-            )),
+                height: 400,
+                width: 300,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    MyTextField(
+                        hintText: 'title',
+                        obscureText: false,
+                        controller: _title),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    MyTextField(
+                        hintText: 'description',
+                        obscureText: false,
+                        controller: _description),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    MyTextField(
+                        hintText: 'venue',
+                        obscureText: false,
+                        controller: _venue),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MyButton(
+                            text: 'Date',
+                            onTap: () => _showDatePicker(context)),
+                        MyButton(
+                            text: 'Time',
+                            onTap: () => _showTimePicker(context)),
+                      ],
+                    )
+                  ],
+                )),
             actions: const [],
           );
         });
